e8e534014921f5fc63c1239e7885a6632e6b5c1f
olenkapokotilo <olenka.pokotilo@gmail.com>
2017-07-23 02:38:30 +0300

    add unity and .testing.dll

--- TestTaskGallery.Tests/UploadFileServiceTest.cs
+++ TestTaskGallery.Tests/UploadFileServiceTest.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using System.Web;
 
 namespace TestTaskGallery.Tests
 {
+    [TestClass]
     public class UploadFileServiceTest
     {
     }
