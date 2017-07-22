a7486f77e684b246d952c7e5660c2cc165967c99
olenkapokotilo <olenka.pokotilo@gmail.com>
2017-07-23 02:09:48 +0300

    add TestTaskGallery.Tests project

--- TestTaskGallery.Tests/UploadFileServiceTest.cs
+++ TestTaskGallery.Tests/UploadFileServiceTest.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestTaskGallery.DataAccess
+namespace TestTaskGallery.Tests
 {
-    public class Class1
+    public class UploadFileServiceTest
     {
     }
 }
